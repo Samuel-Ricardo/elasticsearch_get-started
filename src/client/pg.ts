5acbfcf0bc51d9ff51469eb3bc1e43f8a608fadc
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-16 23:03:40 -0300

    :recycle: refactor: rename - getClinet

--- src/client/pg.ts
+++ src/client/pg.ts
@@ -1,6 +1,6 @@
 import { Client } from "pg"
 
-export const getClinet = () => new Client({
+export const getPGClient = () => new Client({
         host:'IP.DO.BANCO',
         port: 5432,
         database: 'postgres',
