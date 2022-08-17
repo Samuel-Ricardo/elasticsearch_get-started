b3c30375239429500b8ea135f6e65c1c435dbd80
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-16 22:02:23 -0300

    :sparkles: update: route  ('/')

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from 'express'
+import { getClient } from './client/elasticsearch';
 
 const app = express();
 
 app.get('/', async (req:Request, res:Response) => {
-    return res.json('Hello World :D');
+
+    const result = await getClient().index({ 
+        index: 'elasticsearch',
+        type: 'type_elastic_teste',
+        body: {
+            user: 'user',
+            password: '1234',
+            email: 'tomi@email.com',
+        }
+    });
+    
+    return res.json({
+        hello: 'Hello World :D',
+        result
+    });
 })
 
 app.listen(3333, () => console.log(`Running http://localhost:3333`))
