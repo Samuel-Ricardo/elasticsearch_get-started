ee907a988841a80f1d045239c76e8fb60cee34a6
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-16 22:57:19 -0300

    :sparkles: create: fun - getClinet  (./client/pg.ts)

---
+++ src/client/pg.ts
@@ -0,0 +1,11 @@
+import { Client } from "pg"
+
+export const getClinet = () => {
+    return new Client({
+        host:'IP.DO.BANCO',
+        port: 5432,
+        database: 'postgres',
+        password: 'SENHA',
+        user: 'posgres',
+    })
+}
