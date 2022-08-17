27d8099aaf1623bf69bf2a4d8237af348aa8e529
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-08-17 08:31:02 -0300

    :recycle: refact: fun - return

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,5 @@
 export async function getExecutionDuration<TReturn>(fun: Function):Promise<{result:TReturn, durantion: number}> {
     const start = new Date()
     const result:TReturn = await fun()
-    return{result, durantion: new Date().getTime() - start.getTime()} 
+    return{durantion: new Date().getTime() - start.getTime(), result} 
 }
